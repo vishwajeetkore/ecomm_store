72cceb6f0b1b5e691aba4de5d0dadd55a08779da
Vishwajeet Kore <123283352+vishwajeetkore@users.noreply.github.com>
2026-01-13 16:31:47 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a704cc94d0d7649da8a5484c373c0106-283151249.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a2ba84350fb2141c48874e511f6cad03-1792041881.us-east-1.elb.amazonaws.com',
 };
